031be0bb14f7d3a561dbaefe8431a811f83073e5
koulibalyamadou10 <koulibalyamadou10@gmail.com>
2026-01-14 21:15:29 +0000

    first commit

---
+++ mobile_links/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import assetlinks, apple_app_site_association
+
+urlpatterns = [
+    path('.well-known/assetlinks.json', assetlinks),
+    path('.well-known/apple-app-site-association', apple_app_site_association),
+]
---
+++ mobile_links/views.py
@@ -0,0 +1,33 @@
+from django.conf import settings
+from django.http import JsonResponse
+
+def assetlinks(request):
+    return JsonResponse(
+        [
+            {
+                "relation": ["delegate_permission/common.handle_all_urls"],
+                "target": {
+                    "namespace": "android_app",
+                    "package_name": "com.jangogroup.gogames",
+                    "sha256_cert_fingerprints": [
+                        settings.SHA_256  # À remplacer
+                    ]
+                }
+            }
+        ],
+        safe=False
+    )
+
+
+def apple_app_site_association(request):
+    return JsonResponse({
+        "applinks": {
+            "apps": [],
+            "details": [
+                {
+                    "appID": "SSX32PJDA3.com.tinastock.tinastock",
+                    "paths": ["*"]
+                }
+            ]
+        }
+    })
