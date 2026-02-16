dd28e21d4a0fb611b0d67a7253dec63ef2cb1c3b
koulibalyamadou10 <koulibalyamadou10@gmail.com>
2026-02-16 19:25:09 +0000

    Add Android app link relation to assetlinks view for enhanced URL handling and login credential delegation.

--- mobile_links/views.py
+++ mobile_links/views.py
@@ -14,6 +14,19 @@ def assetlinks(request):
                         settings.SHA_256  # À remplacer
                     ]
                 }
+            },
+            {
+                "relation": [
+                "delegate_permission/common.handle_all_urls",
+                "delegate_permission/common.get_login_creds"
+                ],
+                "target": {
+                    "namespace": "android_app",
+                    "package_name": "gn.kinnovate.tinastock_client",
+                    "sha256_cert_fingerprints": [
+                        "04:F8:AB:BE:DD:60:49:4F:60:CE:C6:20:7D:3D:83:FE:FD:F8:89:49:CE:87:AA:54:7F:39:90:FC:C2:3C:BB:1F"
+                    ]
+                }
             }
         ],
         safe=False
